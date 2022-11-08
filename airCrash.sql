0f8a5c4d28bb580710ebf4f143c13d84deced21b
Leon-Espitia <lespitia44@gmail.com>
2022-11-07 20:34:58 -0500

    Drop 'registration' and  'route'

--- airCrash.sql
+++ airCrash.sql
@@ -11,12 +11,17 @@ CREATE TABLE airplanecrash (
     purpose					VARCHAR(150),
     aircraft_type			VARCHAR(150),
     registration			VARCHAR(150),
-    all_aboard				INT,
+    all_aboard				VARCHAR(150),
     passangers_onboard		varchar(100),
     crew_aboard				varchar(100),
     deaths_number			varchar(100),
     passenger_fatalities	varchar(100),
-    crew_fatalities			varchar(100),
-    summary					varchar(300)
+    crew_fatalities			varchar(100)
     
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\PI_03\\airplaneCrashes.csv'
+INTO TABLE airplanecrash
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY ''
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+
+
