1ccf01099353d37e3876994dd4f319ebd4429e44
Leon-Espitia <lespitia44@gmail.com>
2022-11-07 23:10:43 -0500

    Alter table data type

--- airCrash.sql
+++ airCrash.sql
@@ -5,16 +5,16 @@ use Data_Analysis_Project;
 DROP TABLE IF EXISTS airplanecrash;
 CREATE TABLE airplanecrash (
 	crash_Id				INT PRIMARY KEY,
-    crash_date					DATETIME,
+    crash_date				DATE,
     route					VARCHAR(150),
     operator				VARCHAR(150),
     aircraft_type			VARCHAR(150),
-    all_aboard				VARCHAR(150),
-    passangers_onboard		varchar(100),
-    crew_aboard				varchar(100),
-    deaths_number			varchar(100),
-    passenger_fatalities	varchar(100),
-    crew_fatalities			varchar(100)
+    all_aboard				INT,
+    passangers_onboard		INT,
+    crew_aboard				INT,
+    deaths_number			INT,
+    passenger_fatalities	INT,
+    crew_fatalities			INT
     
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\PI_03\\airplaneCrashes.csv'
